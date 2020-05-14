d06c60a8b3f344b6aa81510971296c989ffd03a5
tavares1 <lucastavaresvasconcelos@gmail.com>
2020-05-14 08:48:33 -0300

    Populando a tela com o modelo de forma declarativa.

--- lib/main.dart
+++ lib/main.dart
@@ -8,50 +8,44 @@ main(List<String> args) {
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
   final _transactions = [
     Transaction(
-      id: 't1',
-      title: "Novo Tênis de Corrida",
-      value: 310.40,
-      date: DateTime.now()
-    ),
+        id: 't1',
+        title: "Novo Tênis de Corrida",
+        value: 310.40,
+        date: DateTime.now()),
     Transaction(
-      id: 't2',
-      title: 'Conta de luz',
-      value: 220.30,
-      date: DateTime.now()
-    )
+        id: 't2', title: 'Conta de luz', value: 220.30, date: DateTime.now())
   ];
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-        Container(
-          child: Card(
-            color: Colors.blue,
-            child: Text('Gráfico'),
-            elevation: 5,
-          ),
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
         ),
-        Card(
-          child: Text('Lista de Gastos'),
-        )
-      ],)
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(child: Text(tr.title));
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
