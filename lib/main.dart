111469f1795f574879011b5780a07e00a84b4f80
tavares1 <lucastavaresvasconcelos@gmail.com>
2020-05-14 08:29:49 -0300

    Aprendendo sobre o Main e Cross Axis do Widget Column

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+        Container(
+          child: Card(
+            color: Colors.blue,
+            child: Text('Gráfico'),
+            elevation: 5,
+          ),
+        ),
+        Card(
+          child: Text('Lista de Gastos'),
+        )
+      ],)
+    );
+  }
+}
